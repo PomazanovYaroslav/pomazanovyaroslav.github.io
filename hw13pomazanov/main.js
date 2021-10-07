0917fc654ea5ced65e61e3785c0bdef8efbf1b23
Yaroslav <pomazan120698@gmail.com>
2021-10-07 10:45:40 +0300

    hw13

---
+++ hw13pomazanov/main.js
@@ -0,0 +1,17 @@
+"use strict"
+
+let fruits = ['Apple','Banana','Pineapple'];
+console.log(fruits);
+
+fruits.push('Orange','Grapes');
+console.log(fruits);
+
+
+fruits[1] = 'Peer'
+console.log(fruits);
+
+fruits.pop();
+console.log(fruits);
+
+fruits.unshift('Watermelon');
+console.log(fruits);
