5d8b0b4a29ad875bf71eccc69c743f6bfa6fae06
Yaroslav <pomazan120698@gmail.com>
2021-10-18 13:54:14 +0300

    hw6new

--- hw6pomazanov/index.js
+++ hw6pomazanov/index.js
@@ -1,16 +1,25 @@
-"use strict"
- 
-const login = prompt('Введите логин:',"Ярослав");
-const password = prompt('Введите пароль',"");
-if(login === "Ярослав") {
-  
-   if(password === 'admin1'){
-       alert(`Привет ${login} !`);
-   }
-   else{
-   const password2 = prompt('Введите пароль ещё раз :','');
-        (password2 === 'admin1') ? alert(`Привет ${login}!`) : alert('Пароль не верный !');
-   }
-}else {
-    alert('Не верный логин =(')
+const VALID_LOGIN = 'Admin';
+const VALID_PASSWORD = '123';
+
+const login = prompt('Логин:','');
+
+const isLoginValid = login === VALID_LOGIN;
+let password = ('Пароль:','');
+let isPasswordValid = password === VALID_PASSWORD;
+
+if(!isLoginValid) {
+    console.log('Не правильный логин');
+}else if (isPasswordValid) {
+    console.log('Привет!');
+} else {
+    while (!isPasswordValid) {
+        password = prompt('Пароль:','');
+        if (password === null) break;
+        isPasswordValid = password ===VALID_PASSWORD;
+    }
+    if(isPasswordValid){
+        console.log('Привет!');
+    } else {
+        console.log('Не правильный пароль');
+    }
 }
