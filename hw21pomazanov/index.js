8466296becd57a122d29324fa1f2f7b841793e5c
Yaroslav <pomazan120698@gmail.com>
2021-11-09 19:44:24 +0200

    hw21

---
+++ hw21pomazanov/index.js
@@ -0,0 +1,44 @@
+"use strict"
+
+function clone(o) {
+    const gdcc = "__getDeepCircularCopy__";
+    if (o !== Object(o)) {
+        return o;
+    }
+
+    let set = gdcc in o,
+        cache = o[gdcc],
+        result;
+    if (set && typeof cache == "function") {
+        return cache();
+    }
+
+    o[gdcc] = function() {
+        return result; 
+    };
+
+    if (o instanceof Array) {
+        result = [];
+        for (let i = 0; i < o.length; i++) {
+            result[i] = clone(o[i]);
+        }
+    } else {
+        result = {};
+        for (let prop in o)
+            if (prop != gdcc)
+                result[prop] = clone(o[prop]);
+            else if (set)
+                result[prop] = clone(cache);
+    }
+    if (set) {
+        o[gdcc] = cache;
+    } else {
+        delete o[gdcc];
+    }
+    return result;
+}
+
+let b = clone(a);
+
+b.x = 'b';
+b.nested.y = 'b';
